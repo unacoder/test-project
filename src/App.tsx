fcdce4c66a42707e9a4037bb14c5ea108ff059f5
KiT O <keykakito@gmail.com>
2020-10-17 00:08:45 +0330

    Add pages placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,17 @@
 import React, { useEffect, useState } from "react"
-import logo from "./logo.svg"
-import styled, { keyframes } from "styled-components/macro"
-import { fetchAll } from './api/article'
+import { Route, Switch } from 'react-router'
+import Layout from './pages/Layout'
 
-const StyledApp = styled.div`
-  text-align: center;
-`
-
-const StyledHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`
-
-const StyledLink = styled.a`
-  color: #61dafb;
-`
-
-const KeyframesLogo = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`
-
-const StyledLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${KeyframesLogo} infinite 20s linear;
-  }
-`
+import Articles from './pages/Article/Index'
+import ArticleSingle from './pages/Article/Single'
 
 export default function App() {
-  const [articles, setArticls] = useState([])
-
-  useEffect(() => {
-    fetchAll().then((data) => setArticls(data))
-  }, [])
-
   return (
-    <StyledApp>
-      <StyledHeader>
-        <StyledLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <StyledLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </StyledLink>
-      </StyledHeader>
-    </StyledApp>
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Articles} />
+        <Route path="/:id" exact component={ArticleSingle} />
+      </Switch>
+    </Layout>
   )
 }
---
+++ src/pages/Article/Single.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ArticleSingle: React.FC = (props) => {
+  return <div />
+}
+
+export default ArticleSingle
